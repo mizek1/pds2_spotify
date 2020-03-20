2169e33b8c3d94f486f067f65aa8170793039b5d
mizek1 <daniloalves@aluno.fapce.edu.br>
2020-03-20 20:56:49 -0300

    menu lateral e inicio do rodapé

--- src/main/java/com/fap/spotify/App.java
+++ src/main/java/com/fap/spotify/App.java
@@ -2,12 +2,14 @@
 package com.fap.spotify;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -19,11 +21,54 @@ public class App extends Application {
 	private static Stage stage;
 	private Button btInicio, btBiblioteca, btBusca, btNovaPlaylist, btMusicasCurtidas, btUsuario;
 	private TextField tfBusca;
-	private Label lbSpotify; 
 	
 	public void initComponents() {
+		
 		pane = new BorderPane();
 		pane.setPrefSize(800, 600);		
+		
+		VBox vBoxMenu = initVBoxMenu();
+		pane.setLeft(vBoxMenu);
+		vBoxMenu.setAlignment(Pos.BASELINE_LEFT);
+		
+		HBox hMedia = initHMedia();
+		pane.setBottom(hMedia);
+		hMedia.setAlignment(Pos.BOTTOM_CENTER);
+		
+		
+	}
+	
+	public VBox initVBoxMenu(){
+		
+		VBox vBoxMenu = new VBox(25);
+		
+		Label lbSpotify = new Label("Spotify");
+		vBoxMenu.getChildren().add(lbSpotify);
+		lbSpotify.setAlignment(Pos.BASELINE_CENTER);
+		
+		btInicio = new Button("Início");
+		btBiblioteca = new Button("Minha Biblioteca");
+		btNovaPlaylist = new Button("Nova playlist");
+		btMusicasCurtidas = new Button("Músicas curtidas");
+		
+		btInicio.setPrefWidth(140);
+		btBiblioteca.setPrefWidth(140);
+		btNovaPlaylist.setPrefWidth(140);
+		btMusicasCurtidas.setPrefWidth(140);
+		
+		vBoxMenu.getChildren().addAll(btInicio, btBiblioteca, btNovaPlaylist, btMusicasCurtidas);
+		
+		return vBoxMenu;
+	}
+	
+	public HBox initHMedia() {
+		
+		HBox hMedia = new HBox(15);
+		Label nomeMusica = new Label("Nome da música");
+		Label nomeDisco = new Label("Nome do disco");
+		hMedia.getChildren().addAll(nomeMusica, nomeDisco);
+		
+		return hMedia;
 	}
 	
 	public void initLayout() {
